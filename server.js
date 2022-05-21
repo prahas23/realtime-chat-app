7e48941b7486f77b8d1515599fc30bf688cf6121
Prahas <saiprahas@outlook.com>
2022-05-21 17:05:23 +0530

    Started the backend of the app

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Server is running on the port ${PORT}`));
